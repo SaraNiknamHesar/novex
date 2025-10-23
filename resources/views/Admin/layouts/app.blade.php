026c98cea25f7e10fcf54c721c0ea2b8bbcd061d
SaraNiknamHesar <saraniknaam76@gmail.com>
2025-10-23 16:51:21 +0200

    admin_logout_customization

--- resources/views/Admin/layouts/app.blade.php
+++ resources/views/Admin/layouts/app.blade.php
@@ -72,6 +72,8 @@
 
 
     <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="{{ asset("assets/admin/js/tabler.min.js") }}" defer></script>
     <!-- END GLOBAL MANDATORY SCRIPTS -->
 
--- resources/views/Admin/layouts/sidebar.blade.php
+++ resources/views/Admin/layouts/sidebar.blade.php
@@ -1082,7 +1082,19 @@
                     <a href="#" class="dropdown-item">Feedback</a>
                     <div class="dropdown-divider"></div>
                     <a href="./settings.html" class="dropdown-item">Settings</a>
-                    <a href="./sign-in.html" class="dropdown-item">Logout</a>
+
+                    <a onclick="event.preventDefault();
+                    $('.logout-form').submit();" href="" class="dropdown-item">Logout</a>
+                    <form method="POST" action="{{ route('admin.logout') }}" class="logout-form">
+                        @csrf
+
+                    </form>
+
+
+
+
+
+
                 </div>
             </div>
         </div>
